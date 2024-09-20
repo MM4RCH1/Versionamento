de54232662fe0632d625f7fce41c2f1eaf405748
MINERVA\mateus.santos <mateus.santos@minervafoods.com>
2024-09-20 00:55:49 -0300

    First Commit - CSharp

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NewProjectCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fala Perecim, tudo na Paz?!!");
+
+            Console.Write("5º Termo - Sistemas de Informação - UNIFEB");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Setembro/2024");
+        }
+    }
+}
